7ff70c20e9d2e11fec0ec91de4a227ec44bd5b4e
hannokhakatya <91082973+hannokhakatya@users.noreply.github.com>
2022-05-30 19:20:59 +0200

    PetProjects

--- React/my-app/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ React/my-app/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -4,33 +4,6 @@ import {connect} from 'react-redux'
 
 
 
-
-// const IMyPostsContainer = (props) => {
-
-
-//   return (
-//     <StoreContext.Consumer>
-//       {(store) => {
-//         let state =store.getState();
-//         let addPost = () => {
-//           store.dispatch(addPostActionCreator());
-        
-//         }
-        
-//         let onPostChange = (text) => {
-//           let action = updateNewPostTextActionCreator(text);
-//           store.dispatch(action);
-        
-//         }
-
-//         return <MyPosts updateNewPostText = {onPostChange} addPost={addPost} posts={store.getState().profilePage.posts}
-//     newPostText={store.getState().profilePage.newPostText}/>
-//       }
-//       }
-//     </StoreContext.Consumer>
-//   );
-// };
-
 const mapStateToProps = (state) => {
   return {
     posts:state.profilePage.posts,
